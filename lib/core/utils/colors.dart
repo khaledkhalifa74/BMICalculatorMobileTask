4f6a45a3ef10a80978d3f00cea5431ff9404340e
Khaled Khalifa <96270919+khaledkhalifa74@users.noreply.github.com>
2024-04-20 16:43:32 +0200

    add themes and files

---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xff6ABD45);
+const kSecondaryColor = Color(0xffBCBCBC);
+const kWhiteColor = Colors.white;
+const kBlackColor = Colors.black;
